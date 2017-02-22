51f3552b3703a7863fdb147a6b885e860f3b6ef9
luliang_yu <616625797@qq.com>
2017-02-22 15:50:11 +0800

    Initial commit

---
+++ src/com/arthur/test/HelloWrold.java
@@ -0,0 +1,10 @@
+package com.arthur.test;
+
+/**
+ * Created by Arthur on 2017/2/21.
+ */
+public class HelloWrold {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Arthur");
+    }
+}
